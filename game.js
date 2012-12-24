88e9e6b40aa69f6b9000ad9aae36bee0657d9dde
jacob <jrcurtis@ucsc.edu>
2012-12-24 01:17:41 -0800

    Key and mouse events.

--- game.js
+++ game.js
@@ -44,27 +44,63 @@ var Game = function (spec) {
     this.fps = 30;
     this.dt = 1 / this.fps;
     this.time = 0;
+    this.debug = false;
 
     this.canvas = $("#game-canvas").get(0);
     this.canvas.width = spec.width || 800;
     this.canvas.height = spec.height || 600;
+    this.canvasOffset = $(this.canvas).offset();
     this.ctx = this.canvas.getContext("2d");
 
     this.objects = [];
     this.images = {};
     this.resourcesLoading = 0;
 
+    this.events = {};
+    this.mouseLoc = new Vector();
+
+    this.canvas.onmousedown = function (e) {
+        that.onmousedown(e);
+    };
+    this.canvas.onmouseup = function (e) {
+        that.onmouseup(e);
+    };
+    this.canvas.onmousemove = function (e) {
+        that.onmousemove(e);
+    };
+    this.canvas.parentNode.onkeydown = function (e) {
+        that.onkeydown(e);
+    };
+    this.canvas.parentNode.onkeyup = function (e) {
+        that.onkeyup(e);
+    };
+
     setInterval(function () {
-        that.update(that.dt)
+        that._update(that.dt)
     }, this.dt * 1000);
+};
+
+Game.KEY_PRESSED = 0x1;
+Game.KEY_RELEASED = 0x2;
+Game.KEY_DOWN = 0x4;
+
+Game.prototype._update = function (dt) {
+    if (this.resourcesLoading > 0) {
+        return;
+    }
+
+    this.update(dt);
+
+    var k;
+    for (k in this.events) {
+        if (this.events.hasOwnProperty(k)) {
+            this.events[k] &= ~(Game.KEY_PRESSED | Game.KEY_RELEASED);
+        }
+    }
 };
 
 Game.prototype.update = function (dt) {
     var that = this;
-
-    if (this.resourcesLoading > 0) {
-        return;
-    }
 
     this.time += dt;
     this.ctx.fillStyle = "#000";
@@ -74,7 +110,7 @@ Game.prototype.update = function (dt) {
         o.update(dt, that);
     });
 
-    this.objects.sort(key(function (o) { return o.z; }));
+    this.objects.sort(sortKey(function (o) { return o.z; }));
 
     this.objects.forEach(function (o) {
         o.draw(that);
@@ -87,6 +123,19 @@ Game.prototype.addObject = function (o) {
 
 Game.prototype.addObjects = function (os) {
     this.objects = this.objects.concat(os);
+};
+
+Game.prototype.removeObject = function (o) {
+    var i = this.objects.indexOf(o);
+    if (i >= 0) {
+        this.objects.splice(i, 1);
+    }
+};
+
+Game.prototype.removeObjects = function (os) {
+    os.forEach(function (o) {
+        this.removeObject(o);
+    }, this);
 };
 
 Game.prototype.loadImages = function (paths) {
@@ -102,12 +151,92 @@ Game.prototype.loadImages = function (paths) {
             }
         };
 
-        img.src = path;
         this.resourcesLoading++;
+        img.src = path;
     }, this);
+};
+
+Game.prototype.keyPressed = function (k) {
+    if (this.events.hasOwnProperty(k)) {
+        return (this.events[k] & Game.KEY_PRESSED) !== 0;
+    } else {
+        return false;
+    }
+};
+
+Game.prototype.keyReleased = function (k) {
+    if (this.events.hasOwnProperty(k)) {
+        return (this.events[k] & Game.KEY_RELEASED) !== 0;
+    } else {
+        return false;
+    }
+};
+
+Game.prototype.keyDown = function (k) {
+    if (this.events.hasOwnProperty(k)) {
+        return (this.events[k] & Game.KEY_DOWN) !== 0;
+    } else {
+        return false;
+    }
+};
+
+Game.prototype.keyUp = function (k) {
+    if (this.events.hasOwnProperty(k)) {
+        return (this.events[k] & Game.KEY_DOWN) === 0;
+    } else {
+        return true;
+    }
+};
+
+Game.prototype.onPressEvent = function (key) {
+    if (this.events[key] === undefined
+            || (this.events[key] & Game.KEY_DOWN) === 0) {
+        this.events[key] = Game.KEY_PRESSED | Game.KEY_DOWN;
+    }
+};
+
+Game.prototype.onReleaseEvent = function (key) {
+    this.events[key] = Game.KEY_RELEASED;
+};
+
+Game.prototype.log = function (text) {
+    var size = 20;
+    this.ctx.font = size + "px system,monaco";
+    var meas = this.ctx.measureText(text);
+    this.ctx.fillStyle = "#FFF";
+    this.ctx.fillRect(0, 0, meas.width, size);
+    this.ctx.fillStyle = "#000";
+    this.ctx.fillText(text, 0, size);
+
 };
 
 Game.prototype.onload = function () {
 
 };
+
+Game.prototype.onmousedown = function (e) {
+    this.onPressEvent("BUTTON" + e.button);
+};
+
+Game.prototype.onmouseup = function (e) {
+    this.onReleaseEvent("BUTTON" + e.button);
+};
+
+Game.prototype.onmousemove = function (e) {
+    this.mouseLoc.x = e.pageX - this.canvasOffset.left;
+    this.mouseLoc.y = e.pageY - this.canvasOffset.top;
+};
+
+Game.prototype.onkeydown = function (e) {
+    var key = String.fromCharCode(e.keyCode);
+    this.onPressEvent(key);
+};
+
+Game.prototype.onkeyup = function (e) {
+    var key = String.fromCharCode(e.keyCode);
+    this.onReleaseEvent(key);
+};
+
+
+
 
